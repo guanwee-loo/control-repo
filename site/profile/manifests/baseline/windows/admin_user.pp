4d6a5b26d5a6fb187ce93a4b6de9ea4cba1b9885
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-12 18:06:25 +0800

    add script folder with ACL

--- site/profile/manifests/baseline/windows/admin_user.pp
+++ site/profile/manifests/baseline/windows/admin_user.pp
@@ -1,6 +1,6 @@
 # @summary A short summary of the purpose of this class
 #
-# A description of what this class does
+# Create an admin user and a script folder 
 #
 # @example
 #   include profile::baseline::windows::admin_user
@@ -13,11 +13,27 @@ class profile::baseline::windows::admin_user {
     ensure  => 'present',
     comment => 'Vandelay Admininstrative User',
     groups  => ['Vandelay Industries Administrators','Administrators'],
+    before  => File['C:/adminTools'],
   }
 
+  # Use a 3rd party DSC from the forge for now as I am unable to determine whether the User resource can archieve this
   dsc_userrightsassignment {'Log on as a Service':
     dsc_ensure    => 'Present',
     dsc_identity  => ['Art Vandelay'],
     dsc_privilege => 'SeServiceLogonRight',
+    before        => User['Art Vandelay'],
   }
+
+  file {'C:/adminTools':
+    ensure => directory,
+  }
+
+  acl {'C:/adminTools':
+    permissions => [
+      { identity => 'Art Vandelay', rights => ['full']},
+      { identify => 'Vandelay Industries Administrators', rights => ['read','execute']}
+    ],
+    require     => File['C:/adminTools'],
+  }
+
 }
