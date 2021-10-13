f5cb65fe37664e6e7c4c9dc90c3732e8810e23df
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-13 10:13:09 +0800

    fixed typo error

--- site/profile/manifests/baseline/windows/qol_settings.pp
+++ site/profile/manifests/baseline/windows/qol_settings.pp
@@ -10,7 +10,7 @@ class profile::baseline::windows::qol_settings {
       type   => dword,
       data   => 0,
     }
-    registry_value { 'HKLM\Software\Microsoft\Active Setup\Installed Components\{{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
+    registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
       ensure => present,
       type   => dword,
       data   => 0,
