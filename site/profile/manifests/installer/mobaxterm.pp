16e78c9cb479b466e1a786adcb845ea357919b25
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-13 18:08:06 +0800

    extract the archive

--- site/profile/manifests/installer/mobaxterm.pp
+++ site/profile/manifests/installer/mobaxterm.pp
@@ -1,6 +1,7 @@
 # @summary A short summary of the purpose of this class
 #
-# An example to download and install MobaXTerm
+# An example to using regular Puppet resources to download and install MobaXTerm
+# Using this method requires one to know exact URL of the zip file which consists of a random string of numbers
 #
 # @example
 #   include profile::installer::mobaxterm
@@ -8,6 +9,7 @@ class profile::installer::mobaxterm (String $package_path = '2142021091974654/Mo
   archive { "${profile::baseline::windows::admin_user::script_location}/${package_path}":
     ensure       => present,
     source       => "https://download.mobatek.net//${$package_path}",
+    extract      => true,
     extract_path => $profile::baseline::windows::admin_user::script_location,
   }
 }
