dcaca032e7e07b8e3b3e65ceddc72cf6bea9ec70
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-13 10:36:37 +0800

    Add more settings

--- site/profile/manifests/baseline/windows/qol_settings.pp
+++ site/profile/manifests/baseline/windows/qol_settings.pp
@@ -1,6 +1,9 @@
 # @summary A short summary of the purpose of this class
 #
-# Disable IEESC and enable Windows Shutdown Event Tracker 
+# (1) Disable IEESC 
+# (based on https://docs.microsoft.com/en-us/troubleshoot/browsers/enhanced-security-configuration-faq#how-to-turn-off-internet-explorer-esc-on-windows-servers)
+
+# (2) enable Windows Shutdown Event Tracker 
 #
 # @example
 #   include profile::baseline::windows::qol_settings
@@ -15,5 +18,37 @@ class profile::baseline::windows::qol_settings {
       type   => dword,
       data   => 0,
     }
-
+    registry_value { 'HKLM\SOFTWARE\Wow6432node\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
+      ensure => present,
+      type   => dword,
+      data   => 0,
+    }
+    registry_value { 'HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\IEHarden':
+      ensure => present,
+      type   => dword,
+      data   => 0,
+    }
+    registry_value { 'HKLM\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\IEHarden':
+      ensure => present,
+      type   => dword,
+      data   => 0,
+    }
+    registry_value { 'HKCU\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\IEHarden':
+      ensure => present,
+      type   => dword,
+      data   => 0,
+    }
+    registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\OC Manager\Subcomponents\iehardenadmin':
+      ensure => present,
+      type   => dword,
+      data   => 0,
+    }
+    registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\OC Manager\Subcomponents\iehardenuser':
+      ensure => present,
+      type   => dword,
+      data   => 0,
+    }
+    registry_value { 'HKLM\Software\Microsoft\Internet Explorer\Main\First Home Page':
+      ensure => absent,
+    }
 }
