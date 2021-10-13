349566179fff55ac1e3fdb3df7c1a7f20508e8ab
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-13 17:16:25 +0800

    Use variables

--- site/profile/manifests/baseline/windows/admin_user.pp
+++ site/profile/manifests/baseline/windows/admin_user.pp
@@ -6,35 +6,39 @@
 #   include profile::baseline::windows::admin_user
 class profile::baseline::windows::admin_user {
 
-  group { 'Vandelay Industries Administrators':
+  $admin_user = 'Art Vandelay'
+  $admin_group = 'Vandelay Industries Administrators'
+  $script_location = 'C:/adminTools'
+
+  group { $admin_group:
     ensure => 'present',
   }
 
-  user { 'Art Vandelay':
+  user { $admin_user:
     ensure  => 'present',
     comment => 'Vandelay Admininstrative User',
-    groups  => ['Vandelay Industries Administrators','Administrators'],
-    before  => File['C:/adminTools'],
+    groups  => ["${admin_group}",'Administrators'],
+    before  => File[$script_location],
   }
 
   # Use a 3rd party DSC from the forge for now as I am unable to determine whether the User resource can archieve this
   # Specify the hostname before the user name to avoid re-applying the change in every in Puppet run 
   dsc_userrightsassignment {'Log on as a Service':
-    require       => User['Art Vandelay'],
+    require       => User[$admin_user],
     dsc_ensure    => 'Present',
-    dsc_identity  => ["${facts['hostname']}\\Art Vandelay"],
+    dsc_identity  => ["${facts['hostname']}\\${admin_user}"],
     dsc_privilege => 'SeServiceLogonRight',
   }
 
-  file {'C:/adminTools':
+  file {$script_location:
     ensure  => directory,
   }
 
-  acl {'C:/adminTools':
-    require                    => File['C:/adminTools'],
+  acl {$script_location:
+    require                    => File[$script_location],
     permissions                => [
-      { identity => 'Art Vandelay', rights => ['full']},
-      { identity => 'Vandelay Industries Administrators', rights => ['read','execute']}
+      { identity => $admin_user, rights => ['full']},
+      { identity => $admin_group, rights => ['read','execute']}
     ],
     inherit_parent_permissions => 'false',
   }
