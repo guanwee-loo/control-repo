2072d05cf06fd4b7e7082dd9a72003af89bfbae7
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-13 17:55:41 +0800

    add absolute archive parth

--- site/profile/manifests/installer/mobaxterm.pp
+++ site/profile/manifests/installer/mobaxterm.pp
@@ -4,10 +4,10 @@
 #
 # @example
 #   include profile::installer::mobaxterm
-class profile::installer::mobaxterm {
-  archive { 'MobaXterm':
+class profile::installer::mobaxterm (String $software = 'MobaXterm_Installer_v21.4.zip') {
+  archive { "${profile::baseline::windows::admin_user::script_location}/${software}":
     ensure       => present,
-    source       => 'https://download.mobatek.net/2142021091974654/MobaXterm_Installer_v21.4.zip',
+    source       => "https://download.mobatek.net/2142021091974654/${software}",
     extract_path => $profile::baseline::windows::admin_user::script_location,
   }
 }
