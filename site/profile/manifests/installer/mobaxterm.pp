32794fab5efbe796d13560ec848f2ae98025adc2
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-14 09:10:38 +0800

    cleanup after download

--- site/profile/manifests/installer/mobaxterm.pp
+++ site/profile/manifests/installer/mobaxterm.pp
@@ -6,11 +6,15 @@
 # @example
 #   include profile::installer::mobaxterm
 class profile::installer::mobaxterm (String $package_path = '2142021091974654/MobaXterm_Installer_v21.4.zip') {
+
   $extract_folder = "${profile::baseline::windows::admin_user::script_location}/${split($package_path,'/')[0]}"
+
   archive { "${profile::baseline::windows::admin_user::script_location}/${package_path}":
     ensure       => present,
     source       => "https://download.mobatek.net//${$package_path}",
     extract      => true,
     extract_path => $extract_folder,
+    cleanup      => true,
   }
+
 }
