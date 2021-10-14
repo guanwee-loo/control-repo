0937e8f0d0f985b6c6058d59af5e9bf9fa876496
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-14 10:02:23 +0800

    install msi once extracted

--- site/profile/manifests/installer/mobaxterm.pp
+++ site/profile/manifests/installer/mobaxterm.pp
@@ -1,20 +1,30 @@
 # @summary A short summary of the purpose of this class
 #
 # An example to using regular Puppet resources to download and install MobaXTerm
-# Using this method requires one to know exact URL of the zip file which consists of a random string of numbers
+# Using this method requires one to know the exact URL of the zip file to download from. 
+# The path consists of a random string of numbers
 #
 # @example
 #   include profile::installer::mobaxterm
 class profile::installer::mobaxterm (String $package_path = '2142021091974654/MobaXterm_Installer_v21.4.zip') {
+  $source = "https://download.mobatek.net/${$package_path}"
+  $download_folder = profile::baseline::windows::admin_user::script_location
+  $extract_folder = "${download_folder}/${split($package_path,'/')[0]}"
+  $archive_file = "${download_folder}/${package_path}"
+  $msi_file = "${regsubst($archive_file,'zip','msi')}"
 
-  $extract_folder = "${profile::baseline::windows::admin_user::script_location}/${split($package_path,'/')[0]}"
-
-  archive { "${profile::baseline::windows::admin_user::script_location}/${package_path}":
+  archive { $archive_file:
     ensure       => present,
-    source       => "https://download.mobatek.net//${$package_path}",
+    source       => $source,
     extract      => true,
     extract_path => $extract_folder,
-    cleanup      => true,
+  }
+
+  package { 'MobaXterm':
+    ensure            => installed,
+    source            => $msi_file,
+    installed_options => ['/qn'],
+    subscribe         => Archive[$archive_file],
   }
 
 }
