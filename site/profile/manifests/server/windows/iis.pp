3c63534b13014b01d35f0c1f1f7eb4208a13cee4
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-14 14:53:28 +0800

    delete the default website to prevent port binding conflicet

--- site/profile/manifests/server/windows/iis.pp
+++ site/profile/manifests/server/windows/iis.pp
@@ -10,15 +10,22 @@ class profile::server::windows::iis {
     iis_feature { $iis_features:
       ensure => 'present',
     }
-    iis_site {'basics3':
-      ensure       => 'started',
-      physicalpath => $web_root,
-      require      => File["${web_root}/index.html"],
-    }
 
-    file {"${web_root}/index.html":
-      ensure => 'present',
-      source => 'puppet:///modules/profile/index.html'
-    }
+  # Delete the default website to prevent a port binding conflict.
+  iis_site {'Default Web Site':
+    ensure  => absent,
+    require => Iis_feature['Web-WebServer'],
+  }
+
+  iis_site {'basics3':
+    ensure       => 'started',
+    physicalpath => $web_root,
+    require      => File["${web_root}/index.html"],
+  }
+
+  file {"${web_root}/index.html":
+    ensure => 'present',
+    source => 'puppet:///modules/profile/index.html'
+  }
 
 }
