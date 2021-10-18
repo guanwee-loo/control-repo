d7375917d612f44013b515141cb02044f705a06d
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-18 11:53:31 +0800

    create a top scope variable

--- manifests/site.pp
+++ manifests/site.pp
@@ -24,8 +24,15 @@ File { backup => false }
 # Puppet Enterprise console and External Node Classifiers (ENC's).
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
+
+$variable = 'Hi!'
+class example {
+  notify {"message from elsewhere: ${::variable}": }
+}
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
 }
+
+include example
