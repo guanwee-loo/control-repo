8e5020f6eb13e1072981767d5ab2ba4eb7fcc63f
Guan Wee Loo <guanwee.loo@puppet.com>
2021-10-27 15:59:31 +0800

    fixed syntx error

--- site/profile/manifests/installer/sevenzip.pp
+++ site/profile/manifests/installer/sevenzip.pp
@@ -9,7 +9,7 @@ class profile::installer::sevenzip {
   package { '7zip':
     ensure          => installed,
     provider        => 'chocolatey',
-    install_options => ['-installArgs','"','/qn']
+    install_options => ['-installArgs','"','/qn'],
     notify          => Reboot['after'],
   }
   reboot { 'after' :
