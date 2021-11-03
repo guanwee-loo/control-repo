08ab4aac26b4ecd048079a81902ee0b58ffcdea1
Guan Wee Loo <guanwee.loo@puppet.com>
2021-11-03 17:48:39 +0800

    Add custom fact to return department

---
+++ site/profile/lib/facter/department.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+Facter.add(:department) do
+  # https://puppet.com/docs/puppet/latest/fact_overview.html
+  setcode do
+    hostname=Facter.value(:hostname)
+    dept=hostname.chars.first ?  
+         hostname.chars.first : 
+         'unknown'
+    case 
+    when 'b'
+      'banking'
+    when 'r'
+      'retail'
+    else
+      dept
+    end
+  end
+end
