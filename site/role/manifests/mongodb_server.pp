0f8b68070ae2c6f1b0cb3db2238649501a76d59a
Guan Wee Loo <guanwee.loo@puppet.com>
2021-12-08 18:20:06 +0800

    consolidate data in one place

--- site/role/manifests/mongodb_server.pp
+++ site/role/manifests/mongodb_server.pp
@@ -1,6 +1,6 @@
-# @summary A short summary of the purpose of this class
+# @summary Setup a vanilla mongodb server
 #
-# A description of what this class does
+# Install a default mongodb instance along with the client and db tools
 #
 # @example
 #   include role::mongodb_server
