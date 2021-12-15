1c88552dc237aad176d6d6c40550b5e099bf29f9
CISAT-1174 Loo Guan Wee <guanwee.loo@puppet.com>
2021-12-15 16:07:14 +0800

    managed repo

--- site/role/manifests/postgresql_server.pp
+++ site/role/manifests/postgresql_server.pp
@@ -16,6 +16,7 @@ class role::postgresql_server (
 
   }
   class { 'postgresql::globals':
+    manage_package_repo => true,
     version             => String($version),
   }
   class { 'postgresql::server': 
