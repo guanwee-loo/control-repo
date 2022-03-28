91bdd2a9c681621b4da73b8637fd3d3e2037c155
Loo Guan Wee <guanwee.loo@puppet.com>
2022-03-28 14:41:46 +0800

    fix

--- site/mssql_mock/lib/facter/testusers.rb
+++ site/mssql_mock/lib/facter/testusers.rb
@@ -3,7 +3,7 @@ require 'json'
 Facter.add(:users) do
   # https://puppet.com/docs/puppet/latest/fact_overview.html
   setcode do
-     JSON.parse(File.read('C:/users/Administrator/scb_data/users.txt'))
+     JSON.parse(File.read('C:/users/Administrator/work/scb_data/users.txt'))
   end
 end
 
